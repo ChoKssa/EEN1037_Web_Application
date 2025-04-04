2f2f79d39362380f79a797fafa3953313b513949
Alexandre Cathalifaud <alexandre.cathalifaud@epitech.eu>
2025-04-04 02:27:52 +0100

    add contact form functionality; implement ContactMessage model, admin interface, and form submission handling with success messaging

--- factory_tracking/general/urls.py
+++ factory_tracking/general/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
+    path("submit_contact_form", views.submit_contact_form, name="submit_contact_form"),
     path("about/", views.about, name="about"),
     path("help/", views.help, name="help"),
 ]
--- factory_tracking/general/views.py
+++ factory_tracking/general/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.shortcuts import redirect
+from .models import ContactMessage
 
 
 def index(request):
     context = {}
     return render(request, "general/index.html", context)
 
+
 def contact(request):
     context = {}
     return render(request, "general/contact.html", context)
 
+
+@require_POST
+def submit_contact_form(request):
+    ContactMessage.objects.create(
+        name=request.POST.get("name"),
+        email=request.POST.get("email"),
+        message=request.POST.get("message")
+    )
+
+    messages.success(request, "Your message has been sent successfully. Our support team will get back to you shortly.")
+    return redirect("contact")
+
+
 def about(request):
     context = {}
     return render(request, "general/about.html", context)
+
 
 def help(request):
     context = {}
